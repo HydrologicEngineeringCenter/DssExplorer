0deb932dc1b222157242a319b3f1a47754109382
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2019-08-29 19:12:45 -0700

    basic catalog functionality

---
+++ dss-cmd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dss_cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CMD c = new CMD();
+            if(args.Length ==1)
+            {
+                c = new CMD(args[0]);
+            }
+            c.Run();
+        }
+    }
+}
---
+++ lib/copy-new-libs.bat
@@ -0,0 +1,4 @@
+cd %~dp0
+copy ..\..\dotnet\Output\x64\Debug\*.dll .
+copy ..\..\dotnet\Output\x64\Debug\*.pdb .
+pause
