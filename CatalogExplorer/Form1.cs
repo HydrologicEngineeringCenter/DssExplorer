985800a5bd311b5b5549b9b67c389487d36e6f54
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2019-09-18 16:50:45 -0700

    introducting CatalogExplorer example program

---
+++ CatalogExplorer/Form1.cs
@@ -0,0 +1,39 @@
+﻿using DSSIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatalogExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if( dlg.ShowDialog() == DialogResult.OK)
+            {
+
+                using ( DSSReader r = new DSSReader(dlg.FileName))
+                {
+
+                    DSSPathCollection paths = r.GetCondensedPathNames(true);
+
+                    dataGridView1.DataSource = paths.ToDataTable();
+
+                }
+
+            }
+        }
+    }
+}
