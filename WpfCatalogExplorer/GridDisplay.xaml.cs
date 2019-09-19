2ded4619f63fc4e52291fee1b22360d31cb82f1c
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2019-09-19 09:59:16 -0700

    add wpf simple example

---
+++ WpfCatalogExplorer/GridDisplay.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCatalogExplorer
+{
+    /// <summary>
+    /// Interaction logic for GridDisplay.xaml
+    /// </summary>
+    public partial class GridDisplay : UserControl
+    {
+        public GridDisplay()
+        {
+            InitializeComponent();
+        }
+
+        public DataTable DataTable {
+
+            set {
+                grid1.ItemsSource = value.DefaultView;
+             //  grid1.AutoGenerateColumns = true;
+              //  grid1.CanUserAddRows = false;
+            }
+         }
+    }
+}
---
+++ WpfCatalogExplorer/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using DSSIO;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCatalogExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+
+                using (DSSReader r = new DSSReader(dlg.FileName))
+                {
+
+                    DSSPathCollection paths = r.GetCondensedPathNames(true);
+
+                    grid1.DataTable = paths.ToDataTable();
+
+                }
+
+            }
+        }
+
+        private void Grid1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
