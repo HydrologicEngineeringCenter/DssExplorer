b2561f4a74ee1271ca371b61beff01f16e2639de
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2019-10-08 11:07:03 -0700

    add DssFile class

---
+++ WpfCatalogExplorer/DssFile.cs
@@ -0,0 +1,49 @@
+﻿using DSSIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCatalogExplorer
+{
+  /// <summary>
+  /// Class to read/write DSS file objects
+  /// </summary>
+  public class DssFile
+  {
+    private string _pathToDssFile;
+    private DSSWriter _dssWriter;
+    public DssFile(string pathToDssFile)
+    {
+      _pathToDssFile = pathToDssFile;
+      _dssWriter = new DSSWriter(pathToDssFile);
+      
+    }
+
+    public void Dispose()
+    {
+      _dssWriter.Dispose();
+      GC.SuppressFinalize(this);
+    }
+
+    public DataTable Catalog
+    {
+      get
+      {
+          DSSPathCollection paths = _dssWriter.GetCondensedPathNames(true, true, true);
+          var tbl = paths.ToDataTable();
+        //tbl.Rows.RemoveAt(12);
+        return tbl;
+      }
+    }
+
+    public void Delete(string path)
+    {
+      _dssWriter.DeleteRecord(path);
+    }
+
+
+  }
+}
--- WpfCatalogExplorer/MainWindowVM.cs
+++ WpfCatalogExplorer/MainWindowVM.cs
@@ -1,43 +1,35 @@
-﻿using DSSIO;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace WpfCatalogExplorer
 {
-    class MainWindowVM : System.ComponentModel.INotifyPropertyChanged
-    {
-        private string _filePath;
-        private System.Data.DataTable _table;
-        
-        public string FilePath
-        {
-            get { return _filePath; }
-            set { _filePath = value; NotifyPropertyChanged(); ReadData();  }
-        }
-        public System.Data.DataTable DataTable
-        {
-            get { return _table; }
-        }
-        private void ReadData()
-        {
-            //set datatable;
-            using (DSSReader r = new DSSReader(FilePath))
-            {
-                DSSPathCollection paths = r.GetCondensedPathNames(true, true, true);
-                _table = paths.ToDataTable();
-            }
-            NotifyPropertyChanged(nameof(DataTable));
-            //throw new NotImplementedException();
-        }
+  class MainWindowVM : System.ComponentModel.INotifyPropertyChanged
+  {
+    private string _filePath;
+    private System.Data.DataTable _table;
+    private DssFile _dssFile;
 
-        protected virtual void NotifyPropertyChanged([System.Runtime.CompilerServices.CallerMemberName]string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
+    public string FilePath
+    {
+      get { return _filePath; }
+      set { _filePath = value; NotifyPropertyChanged(); ReadData(); }
     }
+    public System.Data.DataTable DataTable
+    {
+      get { return _table; }
+    }
+    private void ReadData()
+    {
+      //set datatable;
+      _dssFile = new DssFile(FilePath);
+      _table = _dssFile.Catalog;
+      NotifyPropertyChanged(nameof(DataTable));
+      //throw new NotImplementedException();
+    }
+
+    protected virtual void NotifyPropertyChanged([System.Runtime.CompilerServices.CallerMemberName]string propertyName = "")
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+  }
 }
